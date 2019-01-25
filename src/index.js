a9225f9443fb1a07c25eb55ff75406bb1a0f32b4
Joby Joseph <joby.joseph@litmus7.com>
2019-01-25 15:25:34 +0530

    A simple mutation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,60 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const typeDefs = `
-type Query {
-  info: String!
-}
-`;
+// Resolver data for Feed
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  },
+  {
+    id: "link-1",
+    url: "www.google.com",
+    description: "Search engine site"
+  }
+];
 
-const resolvers = {};
+/* How GraphQL resolver. If the resolver returns null for any mandatory fields, it will
+throw error */
+let idCount = links.length;
+
+// We have resolvers for all Query and Mutations in Schema
+const resolvers = {
+  Query: {
+    info: () => `This is API of Hackernews clone`,
+    feed: () => links,
+    link: (parent, args) => links[args.id]
+  },
+  Link: {
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      const link = links[args.id];
+      link.description = args.description;
+      link.url = args.url;
+      return link;
+    },
+    deleteLink: (parent, args) => {
+      const link = links.splice(args.id, 1);
+      return link[0];
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
